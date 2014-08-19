be20bc43f90b6549c21d196271f67fa0d6276c8d
bill-brikiatis <brikiatis@g.harvard.edu>
2014-08-19 16:47:39 -0400

    Create db migrate file

---
+++ app/database/migrations/2014_08_19_203446_plant_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PlantTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('plants', function($table) {
+			
+			$table->increments('id');
+			$table->timestamps();
+			$table->string('plant_name')->unique();
+			$table->integer('indoor_offset_days');
+			$table->integer('outdoor_offset_days');
+			$table->integer('transplant_offset_days');
+			$table->boolean('direct_sow')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('plants');
+	}
+
+}
