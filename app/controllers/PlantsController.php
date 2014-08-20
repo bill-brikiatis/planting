a13a6218e2863b1935c4904ca18da1c915fff138
bill-brikiatis <brikiatis@g.harvard.edu>
2014-08-20 16:43:24 -0400

    Display plant list on home from db

--- app/controllers/PlantsController.php
+++ app/controllers/PlantsController.php
@@ -4,13 +4,19 @@ class PlantsController  extends BaseController {
 	
 	public function postPlantList() {
 			
-		$plant_list = array('Artichoke', 'Arugula', 'Asian Greens', 'Beans', 'Beets', 'Broccoli', 'Broccoli Raab', 
+		/*$plant_list = array('Artichoke', 'Arugula', 'Asian Greens', 'Beans', 'Beets', 'Broccoli', 'Broccoli Raab', 
 					'Brussel Sprouts', 'Cabbage', 'Carrots', 'Cauliflower', 'Collards', 'Corn', 'Corn Salad', 
 					'Cucumbers', 'Eggplants', 'Endive', 'Gourds', 'Kale', 'Kohlrabi', 'Leeks', 'Lettuce', 'Melon', 
 					'Mesclun', 'Microgreens', 'Mustard', 'Okra', 'Onions', 'Parsnips', 'Peas', 'Peppers', 
 					'Pumpkins', 'Radicchio', 'Radish', 'Rhubarb', 'Salad Greens', 'Spinach', 'Squash', 
-					'Swiss Chard', 'Tomatoes', 'Turnips');
-
+					'Swiss Chard', 'Tomatoes', 'Turnips');*/
+					
+					
+		$collection = Plant::all();
+		foreach ($collection as $plant) {
+			$plant_list[] = $plant->plant_name;
+		}
+		
 		return View::make('home')->with('plant_list', $plant_list);
 	}
 }
