5578e36e55816cef57f97b01ca4515cc0d6668ec
bill-brikiatis <brikiatis@g.harvard.edu>
2014-08-25 13:31:11 -0400

    Change box file, added server db connection

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'tunnel.pagodabox.com:3306',
+            'database'  => 'planting',
+            'username'  => 'paz',
+            'password'  => '2UtGS3x1',
+        ),
+
+    ),
+
+);
